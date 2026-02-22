6361ae90a8722facaeb4fea4f7b8586115fa9f62
Priyanshi <priyanshjaini1666@gmail.com>
2026-02-22 23:49:31 +0530

    updated

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema , reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -42,6 +43,17 @@ const validateListing = (req,res,next) => {
         next();
     }
 }
+
+const validateReview = (req,res,next) => {
+    let{error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg= error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+}
+
 //index rouute
 app.get("/listings", wrapAsync (async (req,res) => {
     const allListings = await Listing.find({});
@@ -56,7 +68,7 @@ app.get("/listings/new",(req,res) => {
 //show route
 app.get ("/listings/:id", wrapAsync (async(req,res) =>{
     const {id} = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show", {listing});
 }));
 
@@ -91,6 +103,20 @@ app.delete("/listings/:id",wrapAsync ( async(req,res) => {
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
+}));
+
+//reviews
+//post route
+app.post("/listings/:id/reviews",validateReview ,wrapAsync(async(req,res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
 }));
 
 // app.get("/testlisting", async(req,res) =>{
