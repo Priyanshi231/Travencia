442a598d628bc8c545a66e678b3a7452dc17851e
Priyanshi <priyanshjaini1666@gmail.com>
2026-02-23 11:22:44 +0530

    updated

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema , reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -34,104 +37,8 @@ app.get("/" ,(req,res) => {
     res.send("Hello, World!");
 })
 
-const validateListing = (req,res,next) => {
-    let{error} = listingSchema.validate(req.body);
-    if(error){
-        let errMsg= error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-}
-
-const validateReview = (req,res,next) => {
-    let{error} = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg= error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-}
-
-//index rouute
-app.get("/listings", wrapAsync (async (req,res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index", {allListings});
-}));
-
-//new route
-app.get("/listings/new",(req,res) => {
-    res.render("listings/new");
-})
-
-//show route
-app.get ("/listings/:id", wrapAsync (async(req,res) =>{
-    const {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show", {listing});
-}));
-
-
-//create route
-app.post("/listings",validateListing, wrapAsync (async(req,res,next) => {
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-}));
-
-//edit route
-app.get("/listings/:id/edit",wrapAsync (async (req,res) => {
-    const {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit", {listing});
-}));
-
-//update route
-app.put("/listings/:id",validateListing, wrapAsync (async(req,res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    if(!req.body.listing) {
-        throw new ExpressError(400, "Send valid data for listing");
-    }
-    res.redirect(`/listings/${id}`);
-}));
-
-//delete route
-app.delete("/listings/:id",wrapAsync ( async(req,res) => {
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
-//reviews
-//post route
-app.post("/listings/:id/reviews",validateReview ,wrapAsync(async(req,res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-// app.get("/testlisting", async(req,res) =>{
-//     let sampleListing = new Listing({
-//         title: "My New Villa",
-//         description: "A beautiful villa with sea view",
-//         price: 1200,
-//         location: "Calangute, Goa",
-//         country: "India"
-//     });
-
-//     await sampleListing.save();
-//     console.log("sample saved");
-//     res.send("succesful");
-// })
+app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);
 
 app.use((req,res,next) => {
     next(new ExpressError(404, "Page Not Found"));
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
+const { listingSchema } = require("../schema");
 
 const ListingSchema = new Schema({
   title: {
@@ -25,6 +27,12 @@ const ListingSchema = new Schema({
       ref : "Review",
     }
   ],
+});
+
+ListingSchema.post("findOneAndDelete",async(listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: {$in: listing.reviews } });
+  }
 });
 
 const Listing = mongoose.model("Listing", ListingSchema);
