d69175bb68b4d92219656f5ef33b06fca7cd725a
Priyanshi <priyanshjaini1666@gmail.com>
2026-04-22 23:42:13 +0530

    updated

--- routes/user.js
+++ routes/user.js
@@ -16,8 +16,13 @@ router.post(
       const newuser = new User({ username, email });
       const registeredUser = await User.register(newuser, password);
       console.log(registeredUser);
-      req.flash("success", "Welcome to Wanderlust!");
-      res.redirect("/listings");
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }      
+        req.flash("success", "Welcome to Travencia!");
+        res.redirect("/listings");
+      });  
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("/signup");
