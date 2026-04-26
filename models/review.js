d3e7801ffdf4cffc3afa11147c02198c21c0afec
Priyanshi <priyanshjaini1666@gmail.com>
2026-04-26 23:17:12 +0530

    review authorisation

--- models/review.js
+++ models/review.js
@@ -14,6 +14,10 @@ const ReviewSchema = new Schema({
         type: Date,
         default: Date.now(),
     },
+    author:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
 });
 
 module.exports = mongoose.model("Review", ReviewSchema);
