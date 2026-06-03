2ffb5b13a6e85e925ad6a384803896073e60a3c5
Priyanshi <priyanshjaini1666@gmail.com>
2026-06-04 00:26:05 +0530

    cloud storage

--- routes/listing.js
+++ routes/listing.js
@@ -4,10 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing");
 const { isLoggedIn,isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/lisitngs.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router.route("/")
     .get( wrapAsync (listingController.index))
-    .post(isLoggedIn,validateListing, wrapAsync (listingController.createListing));
+    .post(isLoggedIn,
+        upload.single("listing[image]"),
+        validateListing, 
+        wrapAsync (listingController.createListing)
+    );
 
 //new route
 router.get("/new",isLoggedIn, listingController.renderNewForm);
